da236e2d754ed10154b2a0a30f8b59b983bd6125
Dan Hassin <danhassin@mac.com>
2013-01-19 16:32:56 -0500

    Add PGMidiSession class

---
+++ Sources/PGMidi/PGMidiSession.h
@@ -0,0 +1,58 @@
+//
+//  PGMidiSession.h
+//  PGMidi
+//
+//  Created by Dan Hassin on 1/19/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PGMidi.h"
+
+//These definitions taken directly from VVOpenSource (https://code.google.com/p/vvopensource/)
+//THANK YOU!
+
+//	these are all STATUS MESSAGES: all status mesages have bit 7 set.  ONLY status msgs have bit 7 set to 1!
+//	these status messages go to a specific channel (these are voice messages)
+#define VVMIDINoteOffVal 0x80			//	+2 data bytes
+#define VVMIDINoteOnVal 0x90			//	+2 data bytes
+#define VVMIDIAfterTouchVal 0xA0		//	+2 data bytes
+#define VVMIDIControlChangeVal 0xB0		//	+2 data bytes
+#define VVMIDIProgramChangeVal 0xC0		//	+1 data byte
+#define VVMIDIChannelPressureVal 0xD0	//	+1 data byte
+#define VVMIDIPitchWheelVal 0xE0		//	+2 data bytes
+//	these status messages go anywhere/everywhere
+//	0xF0 - 0xF7		system common messages
+#define VVMIDIBeginSysexDumpVal 0xF0	//	signals the start of a sysex dump; unknown amount of data to follow
+#define VVMIDIMTCQuarterFrameVal 0xF1	//	+1 data byte, rep. time code; 0-127
+#define VVMIDISongPosPointerVal 0xF2	//	+ 2 data bytes, rep. 14-bit val; this is MIDI beat on which to start song.
+#define VVMIDISongSelectVal 0xF3		//	+1 data byte, rep. song number; 0-127
+#define VVMIDIUndefinedCommon1Val 0xF4
+#define VVMIDIUndefinedCommon2Val 0xF5
+#define VVMIDITuneRequestVal 0xF6		//	no data bytes!
+#define VVMIDIEndSysexDumpVal 0xF7		//	signals the end of a sysex dump
+//	0xF8 - 0xFF		system realtime messages
+#define VVMIDIClockVal	 0xF8			//	no data bytes! 24 of these per. quarter note/96 per. measure.
+#define VVMIDITickVal 0xF9				//	no data bytes! when master clock playing back, sends 1 tick every 10ms.
+#define VVMIDIStartVal 0xFA				//	no data bytes!
+#define VVMIDIContinueVal 0xFB			//	no data bytes!
+#define VVMIDIStopVal 0xFC				//	no data bytes!
+#define VVMIDIUndefinedRealtime1Val 0xFD
+#define VVMIDIActiveSenseVal 0xFE		//	no data bytes! sent every 300 ms. to make sure device is active
+#define VVMIDIResetVal	 0xFF			//	no data bytes! never received/don't send!
+
+
+@protocol PGMidiSessionDelegate;
+
+@interface PGMidiSession : NSObject
+
+@property (nonatomic, PGMIDI_DELEGATE_PROPERTY) id<PGMidiSessionDelegate> delegate;
+@property (nonatomic, strong) PGMidi *midi;
+
++ (PGMidiSession *) sharedSession;
+
+@end
+
+@protocol PGMidiSessionDelegate <NSObject>
+@end
+
---
+++ Sources/PGMidi/PGMidiSession.m
@@ -0,0 +1,29 @@
+//
+//  PGMidiSession.m
+//  PGMidi
+//
+//  Created by Dan Hassin on 1/19/13.
+//
+//
+
+#import "PGMidiSession.h"
+
+@implementation PGMidiSession
+{
+}
+
+@synthesize midi, delegate;
+
+static PGMidiSession *shared = nil;
+
++ (PGMidiSession *) sharedSession
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		shared = [[PGMidiSession alloc] init];
+	});
+	
+	return shared;
+}
+
+@end
